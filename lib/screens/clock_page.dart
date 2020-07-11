856163d49a4c981a1afade00e7da9855868a6a3d
Shuvankar Sarkar <sonu041@gmail.com>
2020-07-11 17:06:32 +0530

    Added second progressbar and time difference.

--- lib/screens/clock_page.dart
+++ lib/screens/clock_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:samay/components/reusable_card.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class ClockPage extends StatefulWidget {
   @override
@@ -13,6 +14,14 @@ class _ClockPageState extends State<ClockPage> {
   double seconds; //Not using now but can be used later.
   int defaultHour = 5;
   int defaultMin = 30;
+  int secondHour = -4;
+  int secondMin = 0;
+  int diffHour, diffMin, pickedHour, pickedMin = 0;
+  DateTime diffTime;
+
+  TimeOfDay _time = new TimeOfDay.now();
+  String showTime = 'HH:MM';
+  bool manualOverride = false;
 
   _currentTime({int hour, int min}) {
     Duration duration = new Duration(hours: hour, minutes: min);
@@ -32,11 +41,58 @@ class _ClockPageState extends State<ClockPage> {
     );
   }
 
+  _resetTime() {
+    showTime = _currentTime(hour: defaultHour, min: defaultMin);
+    print('Reset time to ${showTime}');
+    manualOverride = false;
+  }
+
+  Future<Null> selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: _time,
+    );
+    if (picked != null && picked != _time) {
+      print('Time selected : ${picked.toString()}');
+      setState(() {
+        //_time = picked;
+        pickedHour = picked.hour;
+        pickedMin = picked.minute;
+        showTime = "${picked.hour} : ${picked.minute}";
+        manualOverride = true;
+        //TimeOfDay.fromDateTime(DateTime.now()) - picked;
+
+        //diff two TimeOfDay TODO: make it function
+        double _doubleCurrentTime = DateTime.now().hour.toDouble() +
+            (DateTime.now().minute.toDouble() / 60);
+        double _doublePickedTime =
+            picked.hour.toDouble() + (picked.minute.toDouble() / 60);
+
+        double _timeDiff = _doublePickedTime - _doubleCurrentTime;
+        print('timeDiff:' + _timeDiff.toString());
+        diffHour = _timeDiff.truncate();
+        diffMin = ((_timeDiff - _timeDiff.truncate()) * 60).toInt();
+
+        print('Time difference: $diffHour Hour and $diffMin min');
+        //TODO: Fix the precise time difference.
+
+//        Duration difference = picked
+        //diffTime = picked - _current
+        // Time(hour: defaultHour, min: defaultMin);
+      });
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     seconds = DateTime.now().second / 60;
     _triggerUpdate();
+    print('Init called');
+    //_setupTime();
+    showTime = _currentTime(hour: defaultHour, min: defaultMin);
+    diffHour = DateTime.now().hour - defaultHour;
+    diffMin = DateTime.now().minute - defaultMin;
   }
 
   @override
@@ -53,14 +109,22 @@ class _ClockPageState extends State<ClockPage> {
               Expanded(
                 child: ReusableCard(
                   cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Center(
                         child: Text(
-                          _currentTime(hour: defaultHour, min: defaultMin),
-                          style: GoogleFonts.bungee(
-                              fontSize: 100.0,
-                              textStyle: TextStyle(color: Colors.white),
-                              fontWeight: FontWeight.normal),
+                          //_currentTime(hour: defaultHour, min: defaultMin),
+                          manualOverride
+                              ? _currentTime(
+                                  hour: defaultHour + diffHour,
+                                  min: defaultMin + diffMin)
+                              : _currentTime(
+                                  hour: defaultHour, min: defaultMin),
+                          style: TextStyle(fontSize: 100.0),
+//                          style: GoogleFonts.bungee(
+//                              fontSize: 100.0,
+//                              textStyle: TextStyle(color: Colors.white),
+//                              fontWeight: FontWeight.normal),
                         ),
                       ),
                       Center(
@@ -71,25 +135,35 @@ class _ClockPageState extends State<ClockPage> {
                       ),
                     ],
                   ),
+                  onPress: () {
+                    selectTime(context);
+                  },
                   colour: Color(0xFF1D1E33),
                 ),
               ),
               Expanded(
                 child: ReusableCard(
                   cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Center(
                         child: Text(
-                          _currentTime(hour: defaultHour, min: defaultMin),
-                          style: GoogleFonts.bungee(
-                              fontSize: 100.0,
-                              textStyle: TextStyle(color: Colors.white),
-                              fontWeight: FontWeight.normal),
+                          manualOverride
+                              ? _currentTime(
+                                  hour: secondHour + diffHour,
+                                  min: secondMin + diffMin)
+                              : _currentTime(hour: secondHour, min: secondMin),
+
+                          style: TextStyle(fontSize: 100.0),
+//                          style: GoogleFonts.bungee(
+//                              fontSize: 100.0,
+//                              textStyle: TextStyle(color: Colors.white),
+//                              fontWeight: FontWeight.normal),
                         ),
                       ),
                       Center(
                         child: Text(
-                          'USA (EST)',
+                          'USA (ET)',
                           style: TextStyle(fontSize: 20.0),
                         ),
                       ),
@@ -98,10 +172,39 @@ class _ClockPageState extends State<ClockPage> {
                   colour: Color(0xFF1D1E33),
                 ),
               ),
-              Expanded(
-                child: ReusableCard(
-                  cardChild: Text('Time2'),
-                  colour: Color(0xFF1D1E33),
+              SizedBox(
+                height: 30.0,
+              ),
+              LinearPercentIndicator(
+                percent: seconds,
+                lineHeight: 15.0,
+                center: Text(
+                  (seconds * 60).toInt().toString(),
+                  style: new TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+//                lineHeight: 14.0,
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  // Add your onPressed code here!
+                  setState(() {
+                    _resetTime();
+                  });
+                },
+                child: Icon(Icons.refresh),
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+              ),
+              Container(
+                child: Container(
+                  height: 100.0,
                 ),
               ),
             ],
